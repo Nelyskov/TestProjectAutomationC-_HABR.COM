6d6a1b96e18a159b2ba598584e03f29c30b8dab8
DeepSeeker <nlyskv12@gmail.com>
2025-09-08 21:54:32 +0300

    Создание ÐCreated TestBase.cs дÐto control driver

--- TestBase.cs
+++ TestBase.cs
@@ -1,19 +1,41 @@
-﻿namespace TestProjectAutomationC__HABR.COM
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProjectAutomationC__HABR.COM
 {
     /// <summary>
-    /// Базовый класс для управления драйвером и навигацией в браузере
+    /// Базовый класс для управления драйвером и навигации в браузере
+    /// url необходимо инициализировать в .cs файле с тестами (Например, protected override string url => "https://habr.com/";
     /// </summary>
-    public class Tests
+    public abstract class TestBase
     {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+        protected abstract string url { get; }
+
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
-        [Test]
-        public void Test1()
+        public void NavigateTo(string url)
+        { 
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public IWebElement WaitForElement(By locator)
         {
-            Assert.Pass();
+            return wait.Until(e => e.FindElement(locator));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
         }
     }
 }
