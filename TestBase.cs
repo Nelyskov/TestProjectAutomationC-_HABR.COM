f688d608938c51afa5b2a724f93b198ff05608d7
DeepSeeker <nlyskv12@gmail.com>
2025-09-08 21:21:21 +0300

    Init repository

---
+++ TestBase.cs
@@ -0,0 +1,19 @@
+﻿namespace TestProjectAutomationC__HABR.COM
+{
+    /// <summary>
+    /// Базовый класс для управления драйвером и навигацией в браузере
+    /// </summary>
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
